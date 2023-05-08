a151addd9fd1c2e82dfe8385c3c019a3e0eca35d
Patricio-Benglian <6305@holbertonstudents.com>
2023-05-08 11:58:15 -0700

    WIP

---
+++ main.c
@@ -0,0 +1,18 @@
+#include "monty.h"
+
+/**
+ *
+ *
+ */
+
+char *gettoken(void)
+{
+}
+
+/**
+ * main - recieves input, tokenizes, then executes respective function
+ * Return: Always 1.
+ */
+int main(void)
+{
+}
