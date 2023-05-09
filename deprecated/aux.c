48f7e1f49a926d1a725916ad736b198456bac2c9
Patricio-Benglian <6305@holbertonstudents.com>
2023-05-09 04:17:16 -0700

    Moved to folder

---
+++ deprecated/aux.c
@@ -0,0 +1,15 @@
+#include "monty.h"
+
+/**
+ * _push - pushes an element to the stack
+ * @stack: the stack of which to push to
+ * @linenumber: the number stored in the element
+ *
+ */
+void _push(stack_t **stack, unsigned int line_number)
+{
+}
+
+void _pall(stack_t **stack, unsigned int line_number)
+{
+}
