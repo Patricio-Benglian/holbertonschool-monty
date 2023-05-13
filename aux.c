e259a4f1c7de018031a2684572a66cd1c056c084
Patricio-Benglian <6305@holbertonstudents.com>
2023-05-13 11:50:08 -0700

    Undid last push

--- aux.c
+++ aux.c
@@ -77,7 +77,7 @@ void _pop(stack_t **stack, unsigned int line_number)
 
 	stack_t *lec = *stack;
 
-	if (*stack)
+	if (*stack == NULL)
 	{
 		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
 		exit(EXIT_FAILURE);
