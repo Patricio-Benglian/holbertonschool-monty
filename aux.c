1ee4c5082227675816679fb3bcef31d645dfebbb
Patricio-Benglian <6305@holbertonstudents.com>
2023-05-13 12:29:43 -0700

    Fixed pint

--- aux.c
+++ aux.c
@@ -101,7 +101,7 @@ void _swap(stack_t **stack, unsigned int line_number)
 	int val;
 
 	lec = *stack;
-	if (lec == NULL || lec->next == NULL)
+	if (!lec->next || !lec->next->next)
 	{
 		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
