417743715574bf7ab31deb58a2c41108fd412c1a
Patricio-Benglian <6305@holbertonstudents.com>
2023-05-16 04:41:43 -0700

    Betty Corrections

--- aux3.c
+++ aux3.c
@@ -6,17 +6,17 @@
  */
 void _sub(stack_t **stack, unsigned int line_number)
 {
-    stack_t *lec;
-    int diff;
+	stack_t *lec;
+	int diff;
 
-    lec = *stack;
-    if (!(*stack)->next || !(*stack)->next->next)
-    {
-        fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
-        exit(EXIT_FAILURE);
-    }
-    diff = lec->next->n - lec->n;
-    lec = lec->next;
-    _pop(stack, line_number);
-    lec->n = diff;
+	lec = *stack;
+	if (!(*stack)->next || !(*stack)->next->next)
+	{
+		fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	diff = lec->next->n - lec->n;
+	lec = lec->next;
+	_pop(stack, line_number);
+	lec->n = diff;
 }
