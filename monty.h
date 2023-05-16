6beb9508b4e4292da5e949199140ff3bed52b1c7
Patricio-Benglian <6305@holbertonstudents.com>
2023-05-16 04:36:31 -0700

    Added Subtraction

--- monty.h
+++ monty.h
@@ -54,6 +54,7 @@ void _pop(stack_t **stack, unsigned int line_number);
 void _swap(stack_t **stack, unsigned int line_number);
 void _add(stack_t **stack, unsigned int line_number);
 void _nop(stack_t **stack, unsigned int line_number);
+void _sub(stack_t **stack, unsigned int line_number);
 char **gettoken(stack_t **stack);
 int main(int argc, char **argv);
 void arg_get(char *file);
